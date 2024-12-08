25aa1c2e427319b34505ac6c9da7da2f400db19c
Andrei Trunkin <andrei.trunkin@galvintec.com>
2024-12-08 18:22:37 +0100

    chore: set up ESLint and Prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'@typescript-eslint/no-require-imports': 'off',
+			'react/react-in-jsx-scope': 0,
+			'react/jsx-uses-react': 0,
+			'prettier/prettier': 'error',
+		},
+		ignores: ['/.expo', 'node_modules/', 'assets/', 'config/*'],
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+];
