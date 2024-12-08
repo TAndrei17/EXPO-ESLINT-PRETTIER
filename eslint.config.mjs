a1906da2d0bacfc28fbfd0c4886d00d674425c58
Andrei Trunkin <andrei.trunkin@galvintec.com>
2024-12-08 19:17:06 +0100

    chore: update Eslint configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,7 @@ export default [
 			'react/jsx-uses-react': 0,
 			'prettier/prettier': ['error', config],
 		},
-		ignores: ['/.expo', 'node_modules/', 'assets/', 'config/*'],
+		ignores: ['/.expo', 'node_modules/', 'assets/'],
 		settings: {
 			react: {
 				version: 'detect',
